7d364c565a3ae6e1422226df3c196b90d12ac61f
Rashad <rashadatef2@gmail.com>
2025-05-12 02:45:42 +0300

    init push

--- lib/core/widgets/social_login_button.dart
+++ lib/core/widgets/social_login_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_starter_package/core/theme/app_colors.dart';
-import 'dart:math' as math;
+import 'package:flutter_svg/flutter_svg.dart';
 
 enum SocialButtonType { google, apple, facebook }
 
@@ -77,7 +77,11 @@ class SocialLoginButton extends StatelessWidget {
   Widget _getIcon(SocialButtonType type) {
     switch (type) {
       case SocialButtonType.google:
-        return const GoogleLogo();
+        return SvgPicture.asset(
+          'assets/images/google_logo.svg',
+          width: 24,
+          height: 24,
+        );
       case SocialButtonType.apple:
         return const Icon(Icons.apple, color: Colors.black, size: 24);
       case SocialButtonType.facebook:
@@ -124,128 +128,4 @@ class SocialLoginButton extends StatelessWidget {
         return AppColors.mutedLight;
     }
   }
-}
-
-class GoogleLogo extends StatelessWidget {
-  const GoogleLogo({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 24,
-      height: 24,
-      decoration: const BoxDecoration(
-        shape: BoxShape.circle,
-        // color: Colors.white,
-      ),
-      child: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // Multi-colored G
-            SizedBox(
-              width: 18,
-              height: 18,
-              child: CustomPaint(
-                painter: _GoogleGPainter(),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _GoogleGPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final width = size.width;
-    final height = size.height;
-
-    // Colors from Google's brand guidelines
-    const blue = Color(0xFF4285F4);
-    const red = Color(0xFFEA4335);
-    const yellow = Color(0xFFFBBC05);
-    const green = Color(0xFF34A853);
-
-    final paint = Paint()
-      ..style = PaintingStyle.fill;
-
-    // Draw simplified Google G
-    final path = Path();
-
-    // Draw blue half-circle (right side)
-    paint.color = blue;
-    path.moveTo(width * 0.75, height * 0.5);
-    path.arcTo(
-      Rect.fromLTWH(width * 0.25, 0, width * 0.75, height),
-      -math.pi / 2,
-      math.pi,
-      false,
-    );
-    path.lineTo(width * 0.75, height * 0.5);
-    path.close();
-    canvas.drawPath(path, paint);
-
-    // Draw red quarter-circle (top-left)
-    paint.color = red;
-    path.reset();
-    path.moveTo(width * 0.5, height * 0.25);
-    path.arcTo(
-      Rect.fromLTWH(0, 0, width, height * 0.5),
-      math.pi,
-      math.pi / 2,
-      false,
-    );
-    path.lineTo(width * 0.5, height * 0.25);
-    path.close();
-    canvas.drawPath(path, paint);
-
-    // Draw yellow quarter-circle (bottom-left)
-    paint.color = yellow;
-    path.reset();
-    path.moveTo(width * 0.25, height * 0.5);
-    path.arcTo(
-      Rect.fromLTWH(0, height * 0.5, width * 0.5, height * 0.5),
-      -math.pi / 2,
-      math.pi / 2,
-      false,
-    );
-    path.lineTo(width * 0.25, height * 0.5);
-    path.close();
-    canvas.drawPath(path, paint);
-
-    // Draw green quarter-circle (bottom-right)
-    paint.color = green;
-    path.reset();
-    path.moveTo(width * 0.75, height * 0.75);
-    path.arcTo(
-      Rect.fromLTWH(width * 0.5, height * 0.5, width * 0.5, height * 0.5),
-      math.pi,
-      math.pi / 2,
-      false,
-    );
-    path.lineTo(width * 0.75, height * 0.75);
-    path.close();
-    canvas.drawPath(path, paint);
-
-    // White center
-    paint.color = Colors.white;
-    canvas.drawCircle(
-      Offset(width * 0.5, height * 0.5),
-      width * 0.25,
-      paint,
-    );
-
-    // Blue bar on right
-    paint.color = blue;
-    canvas.drawRect(
-      Rect.fromLTWH(width * 0.5, height * 0.4, width * 0.5, height * 0.2),
-      paint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 } 
